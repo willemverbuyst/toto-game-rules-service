90c8e8a6d0b12855b3e0670bca61ebca865a5ba8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-10-18 19:53:28 +0200

    ci: work on docker

--- api/configs/env.go
+++ api/configs/env.go
@@ -14,11 +14,10 @@ func EnvMongoURI() string {
 		log.Fatal("Error loading .env file")
 	}
 
-	user := os.Getenv("MONGO_USER")
-	password := os.Getenv("MONGO_PASSWORD")
 	db := os.Getenv("MONGO_INITDB_DATABASE")
 
-	return fmt.Sprintf("mongodb://%s:%s@mongodb:27017/%s?authSource=admin", user, password, db)
+	return fmt.Sprintf("mongodb://localhost:27017/%s", db)
+
 }
 
 func EnvMongoDB() string {
